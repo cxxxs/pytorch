7d7ada1b3348410a397d2d479e67b59aa0da48e1
Jane Xu <janeyx@meta.com>
2023-10-13 13:56:44 -0700

    Make step() faster by passing in a tensor vs scalar 1
    ghstack-source-id: 454df8b1fd06cf9d5b69f39b4d9db6fee7d0f282
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/111084

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -225,7 +225,6 @@ def _register_foreach_lowering(aten_fn, decomp_fn):
 
     @functools.wraps(decomp_fn)
     def wrapped(*args, **kwargs):
-        assert len(args) <= 2
         out = decomp_fn(*args, **kwargs)
         validate_ir(out)
         return out
@@ -4733,6 +4732,7 @@ register_pointwise_numeric(aten.nextafter)
 
 register_foreach_pointwise(aten._foreach_add.List, add, allow_alpha=True)
 register_foreach_pointwise(aten._foreach_add.Scalar, add, allow_alpha=True)
+register_foreach_pointwise(aten._foreach_add.Tensor, add, allow_alpha=True)
 register_foreach_pointwise(aten._foreach_mul.List, mul)
 register_foreach_pointwise(aten._foreach_mul.Scalar, mul)
 register_foreach_pointwise(aten._foreach_sub.List, sub)
